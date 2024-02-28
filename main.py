fa180c34bf6a78acbde0cff820c9e10f320fe7da
MaTT <mat1297c@gmail.com>
2024-02-28 12:08:29 -0500

    agregando implementacion de funciones

--- main.py
+++ main.py
@@ -6,9 +6,13 @@ Seleccione una operación
 2. Resta
 3. Division
 """)
+a= input("ingrese el primer numero ")
+b= input("ingrese el segundo numero ")
 
 try:
     option = int(option)
+    a= int(a)
+    b=int(b)
 except:
     print ("valor no valido")
 
@@ -16,4 +20,10 @@ if option not in [1,2,3]:
     print ("Valor no valido")
     exit()
 
+if option== 1:
+    print(suma(a,b))
+elif option == 2:
+    print(resta(a,b))
+else :
+    print(division(a,b))
 
